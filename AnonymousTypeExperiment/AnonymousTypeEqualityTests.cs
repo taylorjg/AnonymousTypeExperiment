49f39d5155e113942171c82b2c3b9a64e0b4c4f8
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-09 22:59:43 +0100

    Initial commit

---
+++ AnonymousTypeExperiment/AnonymousTypeEqualityTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace AnonymousTypeExperiment
+{
+    // ReSharper disable InconsistentNaming
+
+    [TestFixture]
+    internal class AnonymousTypeEqualityTests
+    {
+        [Test]
+        public void ObjectEquals_GivenTwoEquivalentInstancesOfAnAnonymousType_ReturnsTrue()
+        {
+            // Arrange
+            var object1 = new {Prop1 = "ABC", Prop2 = 123};
+            var object2 = new {Prop1 = "ABC", Prop2 = 123};
+
+            // Act
+            var actual = object1.Equals(object2);
+
+            // Assert
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void EqualityOperator_GivenTwoEquivalentInstancesOfAnAnonymousType_ReturnsFalse()
+        {
+            // Arrange
+            var object1 = new { Prop1 = "ABC", Prop2 = 123 };
+            var object2 = new { Prop1 = "ABC", Prop2 = 123 };
+
+            // Act
+            var actual = object1 == object2;
+
+            // Assert
+            Assert.That(actual, Is.False);
+        }
+
+        [Test]
+        public void ReferenceEquals_GivenTwoEquivalentInstancesOfAnAnonymousType_ReturnsFalse()
+        {
+            // Arrange
+            var object1 = new { Prop1 = "ABC", Prop2 = 123 };
+            var object2 = new { Prop1 = "ABC", Prop2 = 123 };
+
+            // Act
+            var actual = ReferenceEquals(object1, object2);
+
+            // Assert
+            Assert.That(actual, Is.False);
+        }
+    }
+}
